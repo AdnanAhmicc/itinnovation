4ae9952ca2e48036ecf7868562588143137aafda
Adnan Ahmic <130772901+AdnanAhmicc@users.noreply.github.com>
2023-06-24 14:48:57 +0200

    Add files via upload

---
+++ src/data/servicesData.jsx
@@ -0,0 +1,44 @@
+const servicesData = [
+  {
+    title: 'Telefoni',
+    icon: '@lottie/telefoni-icon.json',
+    link: '/telefoni'
+  },
+  {
+    title: 'El',
+    icon: '@lottie/el-icon.json',
+    link: '/el'
+  },
+  {
+    title: 'Leasing',
+    icon: '@lottie/leasing-icon.json',
+    link: '/leasing'
+  },
+  {
+    title: 'Bredband',
+    icon: '@lottie/bredband-icon.json',
+    link: '/bredband'
+  },
+  {
+    title: 'Molnväxel',
+    icon: '@lottie/cloudpbx-icon.json',
+    link: '/molnvaxel'
+  },
+  {
+    title: 'Kontakt center',
+    icon: '@lottie/kontaktcenter-icon.json',
+    link: '/omnichannel'
+  },
+  {
+    title: 'Marknadsförning',
+    icon: '@lottie/socialmediamarketing-icon.json',
+    link: '/marknadsforning'
+  },
+  {
+    title: 'Webbutveckling',
+    icon: '@lottie/webbutveckling-new-icon.json',
+    link: '/webbutveckling'
+  }
+];
+
+export default servicesData;
